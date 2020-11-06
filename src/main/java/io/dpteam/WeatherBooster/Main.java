c7d01b34428bb92c727be552d2081dbd8576d01b
DartPower <>
2020-11-06 16:23:19 +0300

    Zalil

---
+++ src/main/java/io/dpteam/WeatherBooster/Main.java
@@ -0,0 +1,33 @@
+package io.dpteam.WeatherBooster;
+
+import java.util.Iterator;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Runnable {
+	public WeatherBooster() {
+		super();
+	}
+
+	public void run() {
+		Iterator var2 = Bukkit.getWorlds().iterator();
+
+		while(var2.hasNext()) {
+			World world = (World)var2.next();
+			if (world.hasStorm()) {
+				world.setStorm(false);
+			}
+		}
+	}
+
+	public void onEnable() {
+		System.out.println("WeatherBooster Enabled");
+		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 20L, 200L);
+	}
+
+	@Override
+	public void onDisable() {
+		System.out.println("WeatherBooster Disabled");
+	}
+}
